f63b2f406c6cff09dd030d331463eef397324b8b
alexisjcarr <acarr59@gmail.com>
2019-07-12 10:48:01 -0500

    ✨ Adds project POST.
     #1

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -27,6 +27,36 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router
+router.get("/:id/actions", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const projectActions = await db.getProjectActions(id);
+    if (!projectActions) {
+      res.status(404).json({
+        error: `Project with id ${id} doesn't exist.`
+      });
+    } else {
+      res.status(200).json(projectActions);
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({
+      error: "Please enter a name and a description for the project."
+    });
+  } else {
+    try {
+      const project = await db.insert(req.body);
+      res.status(201).json(project);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+});
 
 module.exports = router;
